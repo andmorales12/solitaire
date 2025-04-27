1a9d144395e54dffa939b9094146d9fafc80b6c2
Andrea Morales <moralesa7412@gmail.com>
2025-04-27 11:31:41 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,3 +6,40 @@
 
 io.stdout:setvbuf("no")
 
+Card = {}
+
+function Card:new(suit, value)
+    local newCard = {
+        suit = suit,
+        value = value,
+        img = love.graphics.newImage("assets/card_"..suit.."_"..value..".png"),
+        faceUp = false,
+        x = 0,
+        y = 0,
+        width = 64,
+        height = 64,
+        dragging = false,
+        offsetX = 0,
+        offsetY = 0,
+    }
+    setmetatable(newCard, self)
+    self.__index = self
+    return newCard
+end
+
+function Card:draw()
+    if self.faceUp then
+        love.graphics.draw(self.img, self.x, self.y)
+    else
+        local back = love.graphics.newImage("assets/card_back.png")
+        love.graphics.draw(back, self.x, self.y)
+    end
+end
+
+function Card:isHovered(mx, my)
+    return mx >= self.x and mx <= self.x + self.width
+       and my >= self.y and my <= self.y + self.height
+end
+
+return Card
+
