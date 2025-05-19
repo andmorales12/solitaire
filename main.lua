092a53e3d0f80e20ddd886979eeecd0a1b93df5e
Andrea Morales <moralesa7412@gmail.com>
2025-05-19 00:00:27 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,11 @@
 -- Andrea Morales Villegas
--- CMPM 121 - Project 1: Solitaire
--- 04/25/2025
+-- CMPM 121 - Project 2: Solitaire, but better
 
 -- file: main.lua
 
 io.stdout:setvbuf("no")
+
+GAME_TITLE = "Solitaire"
 
 local CardsLoad = require "cardsload"
 local CardPiles = require "cardpiles"
@@ -12,19 +13,47 @@ local Controls = require "controls"
 
 local cardPiles
 local controls
+local win = false
+local resetButton = { x = 700, y = 500, w = 100, h = 40 }
 
 function love.load()
-    love.graphics.setBackgroundColor(0, 0.5, 0) -- load game background color
-
+    love.graphics.setBackgroundColor(0, 0.5, 0)
     cardPiles = CardPiles:new()
     cardPiles:init()
-
     controls = Controls:new(cardPiles)
 end
 
 function love.update(dt)
-    -- for movement
     controls:update(dt)
+    if cardPiles:checkWin() then
+        win = true
+    end
+end
+
+function love.draw()
+    cardPiles:draw()
+    if win then
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.printf("You Win!", 0, 50, love.graphics.getWidth(), "center")
+    end
+    -- Draw reset button
+    love.graphics.setColor(0.8, 0.1, 0.1)
+    love.graphics.rectangle("fill", resetButton.x, resetButton.y, resetButton.w, resetButton.h)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.printf("Reset", resetButton.x, resetButton.y + 10, resetButton.w, "center")
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if x > resetButton.x and x < resetButton.x + resetButton.w and y > resetButton.y and y < resetButton.y + resetButton.h then
+        cardPiles:init()
+        win = false
+    else
+        controls:mousepressed(x, y, button)
+    end
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+    controls:mousereleased(x, y, button)
 end
 
 function love.draw()
